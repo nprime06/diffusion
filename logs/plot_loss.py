0dbfc7b3aae04de2e1fdeed8f585da158515052b
nprime06 <78390096+nprime06@users.noreply.github.com>
2025-12-20 12:05:09 -0800

    create loss graph

---
+++ logs/plot_loss.py
@@ -0,0 +1,32 @@
+import json
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+RUN_DIR = "/Users/william/Desktop/Random/diffusion/logs/ddpm/mnist_ddpm_unet_2025-12-20_13-49-49"
+
+CUTOFF = 10000
+
+steps = []
+losses = []
+
+LOSS_DIR = os.path.join(RUN_DIR, "metrics")
+FILE_PATH = os.path.join(LOSS_DIR, "loss.jsonl")
+
+with open(FILE_PATH, "r") as f:
+    for line in f:
+        data = json.loads(line)
+        steps.append(data["step"])
+        losses.append(data["loss"])
+
+steps = np.array(steps)
+losses = np.array(losses)
+
+steps = steps[:CUTOFF]
+losses = losses[:CUTOFF]
+
+plt.plot(steps, losses)
+plt.xlabel("Step")
+plt.ylabel("Loss")
+plt.title("Training (cutoff: " + str(CUTOFF) + ")")
+plt.savefig(os.path.join(LOSS_DIR, "training_cutoff_" + str(CUTOFF) + ".png"))
