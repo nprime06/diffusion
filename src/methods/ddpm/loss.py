autocast and matmul precision fix

src/methods/ddpm/loss.py
@@ -10,7 +10,7 @@ def loss(model, x0, scheduler): # x0: (B, C, H, W)
     eps = torch.randn_like(x0)
     xt = torch.sqrt(alpha_prod) * x0 + torch.sqrt(1.0 - alpha_prod) * eps
     std_t = scheduler.get_standardized_time(t).to(device=x0.device)
-    with autocast(device_type=x0.device.type, dtype=torch.bfloat16):
+    with torch.autocast(device_type=x0.device.type, dtype=torch.bfloat16):
         pred_eps = model(xt, std_t)
         loss = F.mse_loss(pred_eps, eps)
     return loss