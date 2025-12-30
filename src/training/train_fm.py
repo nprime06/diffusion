4d61f68367341493d610514d2cd2d208a953e884
nprime06 <78390096+nprime06@users.noreply.github.com>
2025-12-29 19:39:57 -0800

    balls

--- src/training/train_fm.py
+++ src/training/train_fm.py
@@ -6,7 +6,7 @@ from methods.fm.loss import loss
 from methods.fm.sampler import sample
 from run_io import save_checkpoint, flush_losses, save_samples_gif
 
-def save_logs(run_dir, loss_buffer, step, model, optimizer, fm_config, device, images_mean, images_std, num_samples=100):
+def save_logs(run_dir, loss_buffer, step, model, optimizer, fm_config, device, image_shape, images_mean, images_std, num_samples=100):
     loss_path = os.path.join(run_dir, "metrics", "loss.jsonl")
     checkpoint_dir = os.path.join(run_dir, "checkpoints")
     samples_dir = os.path.join(run_dir, "samples")
@@ -14,16 +14,16 @@ def save_logs(run_dir, loss_buffer, step, model, optimizer, fm_config, device, i
     flush_losses(loss_path, loss_buffer)
     save_checkpoint(checkpoint_dir, step, model, optimizer)
 
-    x0 = torch.randn(num_samples, 1, 28, 28, device=device)
+    x0 = torch.randn(num_samples, *image_shape, device=device)
     c = torch.arange(10, device=device, dtype=torch.long).repeat_interleave(10) + 1
     cfg_scale = torch.arange(10, device=device, dtype=torch.float).repeat(10) # samples will have rows c = 0, 1, ..., 9, and cols cfg_scale = 0, 1, ..., 9
-    samples = sample(model, x0, c, cfg_scale, fm_config) # history list from x0 to xT; (T, N, 1, 28, 28)
-    samples = (samples * images_std.to(device).reshape(1, 1, 1, 28, 28)) + images_mean.to(device).reshape(1, 1, 1, 28, 28)
+    samples = sample(model, x0, c, cfg_scale, fm_config) # history list from x0 to xT; (T, N, C, H, W)
+    samples = (samples * images_std.to(device).reshape(1, 1, *image_shape)) + images_mean.to(device).reshape(1, 1, *image_shape)
     save_samples_gif(samples_dir, step, samples)
 
     return
 
-def train_fm(model, dataloader, fm_config, train_config, device, images_mean, images_std): # dataloader on cpu; model on device
+def train_fm(model, dataloader, fm_config, train_config, device, image_shape, images_mean, images_std): # dataloader on cpu; model on device
     params_list = [p for p in model.parameters() if p.requires_grad]
     decay_params = [p for p in params_list if p.dim() >= 2]
     nodecay_params = [p for p in params_list if p.dim() < 2]
@@ -38,7 +38,7 @@ def train_fm(model, dataloader, fm_config, train_config, device, images_mean, im
     while step < train_config.max_steps:
         for images, labels in dataloader:
             t0 = time.time()
-            images, labels = images.to(device).reshape(-1, 1, 28, 28), labels.to(device)
+            images, labels = images.to(device).reshape(-1, *image_shape), labels.to(device)
             loss_val = loss(model, images, labels, train_config.cfg_proportion)
 
             optimizer.zero_grad()
@@ -52,13 +52,13 @@ def train_fm(model, dataloader, fm_config, train_config, device, images_mean, im
             loss_buffer.append({"step": step, "loss": loss_item, "lr": optimizer.param_groups[0]["lr"], "time": f"{1000 * (t1 - t0):.2f}ms"})
 
             if step % train_config.early_checkpoint_every == 0 and step < train_config.num_early_checkpoints * train_config.early_checkpoint_every:
-                save_logs(train_config.run_dir, loss_buffer, step, model, optimizer, fm_config, device, images_mean, images_std)
+                save_logs(train_config.run_dir, loss_buffer, step, model, optimizer, fm_config, device, image_shape, images_mean, images_std)
             elif step % train_config.late_checkpoint_every == 0:
-                save_logs(train_config.run_dir, loss_buffer, step, model, optimizer, fm_config, device, images_mean, images_std)
-                
+                save_logs(train_config.run_dir, loss_buffer, step, model, optimizer, fm_config, device, image_shape, images_mean, images_std)
+            
             step += 1
             if step >= train_config.max_steps: break
     
-    save_logs(train_config.run_dir, loss_buffer, step, model, optimizer, fm_config, device, images_mean, images_std)
+    save_logs(train_config.run_dir, loss_buffer, step, model, optimizer, fm_config, device, image_shape, images_mean, images_std)
 
     return model
