5fd2d5b8cdeac0517fdd89f1f5a0d8c567ec78a7
nprime06 <78390096+nprime06@users.noreply.github.com>
2025-12-31 21:04:18 -0800

    afhq dit run

--- src/main.py
+++ src/main.py
@@ -34,12 +34,12 @@ class TrainConfig:
     learning_rate: float = 3e-4
     weight_decay: float = 1e-2
     grad_clip_norm: float = 1.0
-    max_steps: int = 50000
+    max_steps: int = 1000
     batch_size: int = 128
     cfg_proportion: float = 0.8
     run_dir: str = args.run_dir
     early_checkpoint_every: int = 500 # steps
-    num_early_checkpoints: int = 0
+    num_early_checkpoints: int = 3
     late_checkpoint_every: int = 25000 # steps
 
 train_config = TrainConfig()
@@ -145,11 +145,11 @@ elif args.backbone == 'dit':
     @dataclass
     class DiTConfig:
         image_shape: tuple[int, int, int]
-        d_model: int = 256
+        d_model: int = 512
         d_embed: int = 128
         heads: int = 8
-        num_layers: int = 6
-        p: int = 4
+        num_layers: int = 12
+        p: int = 8
         cross_attn: bool = False
     dit_config = DiTConfig(image_shape=image_shape)
     run_info["ditconfig"] = asdict(dit_config)
